34a4466e3b26ffdf2da8df690cf018b105be6bba
adi5689 <adityakumarsahu.89@gmail.com>
2024-12-18 17:12:55 +0530

    updates

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
 interface ImageSliderProps {
